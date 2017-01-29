3e5b03c7c9f3da7b707411d5b5f5abb58b5c7ed9
SnowPardik <snow_leopard@hmamail.com>
2017-01-29 22:29:17 +0200

    definition of the toggle button is moved to the separate method

--- src/com/mouse/backlightswitch/MainActivity.java
+++ src/com/mouse/backlightswitch/MainActivity.java
@@ -11,16 +11,21 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		ToggleButton toggle = (ToggleButton) findViewById(R.id.toggle_button_switch);
-		toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-			if(isChecked){
-				Toast.makeText(getApplicationContext(), R.string.message_off, Toast.LENGTH_SHORT).show();;
-			} else{
-				Toast.makeText(getApplicationContext(), R.string.message_on, Toast.LENGTH_SHORT).show();;
-			}
-		}
-		});	
+		setContentView(R.layout.activity_main);				
+defineToggleButton();		
 	}
+
+private void defineToggleButton() {
+	ToggleButton toggle = (ToggleButton) findViewById(R.id.toggle_button_switch);
+toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		if(isChecked){
+			Toast.makeText(getApplicationContext(), R.string.message_off, Toast.LENGTH_SHORT).show();;
+		} else{
+			Toast.makeText(getApplicationContext(), R.string.message_on, Toast.LENGTH_SHORT).show();;
+		}
+	}
+	});	
+
+}
 }
