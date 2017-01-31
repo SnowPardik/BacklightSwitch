0c686bf8ec4f40c2bb54308e45323cd2790b5bfe
SnowPardik <snow_leopard@hmamail.com>
2017-01-31 19:01:16 +0200

    toggle button's condition is set in dependence on actual status of the backlight

--- src/com/mouse/backlightswitch/MainActivity.java
+++ src/com/mouse/backlightswitch/MainActivity.java
@@ -11,11 +11,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);				
-defineToggleButton();		
+		setContentView(R.layout.activity_main);
+		setActionOnToggleButtonCheckChanged();		
 	}
 
-private void defineToggleButton() {
+private void setActionOnToggleButtonCheckChanged() {
 	ToggleButton toggle = (ToggleButton) findViewById(R.id.toggle_button_switch);
 toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -28,13 +28,34 @@ toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 }
 
 private void onChecked() {
-	Toast.makeText(getApplicationContext(), R.string.message_off, Toast.LENGTH_SHORT).show();
 	switchBacklightOff();
+	Toast.makeText(getApplicationContext(), R.string.message_off, Toast.LENGTH_SHORT).show();
 }
 
 private void onUnchecked() {
-	Toast.makeText(getApplicationContext(), R.string.message_on, Toast.LENGTH_SHORT).show();
 	switchBacklightOn();
+	Toast.makeText(getApplicationContext(), R.string.message_on, Toast.LENGTH_SHORT).show();	
+}
+
+@Override
+protected void onStart() {
+	super.onStart();
+isBacklightActuallyOn();
+}
+
+private boolean isBacklightActuallyOn() {
+	return true;
+}
+
+@Override
+protected void onResume() {
+	super.onResume();
+	setChecked();
+}
+
+private void setChecked() {
+	ToggleButton toggle = (ToggleButton) findViewById(R.id.toggle_button_switch);
+	toggle.setChecked(!isBacklightActuallyOn());
 }
 
 private void switchBacklightOff() {
