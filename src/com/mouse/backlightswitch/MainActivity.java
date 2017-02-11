6b407d5436d5a6c090d9b2af8a9c7f734a5e61d0
SnowPardik <snow_leopard@hmamail.com>
2017-02-11 23:01:46 +0200

    a method for getting a current brightness level of the backlight is added

--- src/com/mouse/backlightswitch/MainActivity.java
+++ src/com/mouse/backlightswitch/MainActivity.java
@@ -1,8 +1,13 @@
 package com.mouse.backlightswitch;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -12,10 +17,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		onCheckChanged();		
-}
+		//TextView textView = (TextView) findViewById(R.id.brightness_level);
+		//textView.setText("Brightness level");
+		setToggleListener();		
+	}
 
-	private void onCheckChanged() {
+	private void setToggleListener() {
 		ToggleButton toggle = (ToggleButton) findViewById(R.id.toggle_button_switch);
 	toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -46,17 +53,42 @@ public class MainActivity extends Activity {
 @Override
 protected void onStart() {
 	super.onStart();
-isBacklightActuallyOn();
+isBacklightCurrentlyOn();
 setChecked();
 }
 
-private boolean isBacklightActuallyOn() {
-	return true;
+private boolean isBacklightCurrentlyOn() {
+return true;
 }
 
 private void setChecked() {
 	ToggleButton toggle = (ToggleButton) findViewById(R.id.toggle_button_switch);
-	toggle.setChecked(!isBacklightActuallyOn());
-}
+	toggle.setChecked(!isBacklightCurrentlyOn());
 }
 
+@Override
+protected void onResume() {
+	super.onResume();
+	getCurrentBrightnessLevel();
+}
+
+private void getCurrentBrightnessLevel() {
+	String path = "/sys/class/leds/wled:backlight/brightness";
+File f = new File(path);
+StringBuilder sb = new StringBuilder();
+String s;
+String brightnessLevel;
+try {
+FileReader fr = new FileReader(f);
+BufferedReader br = new BufferedReader(fr);
+s = br.readLine();
+br.close();
+		sb.append(s);
+brightnessLevel = sb.toString();
+TextView textView = (TextView) findViewById(R.id.brightness_level);
+textView.setText(brightnessLevel);
+} catch (Exception e) {
+	// TODO: handle exception
+}
+}
+}
