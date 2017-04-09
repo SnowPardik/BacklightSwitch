206481cf4407b0ae788d6924fee3342275933b94
SnowPardik <snow_leopard@hmamail.com>
2017-04-09 20:51:05 +0300

    text of the first created notification depends on the backlight state

--- src/com/mouse/backlightswitch/BacklightSwitchService.java
+++ src/com/mouse/backlightswitch/BacklightSwitchService.java
@@ -22,32 +22,38 @@ public class BacklightSwitchService extends Service {
 	private static final int NOTIFICATION_ID = 1;
 	private static final String NOTIFICATION_ACTION = "com.mouse.backlightswitch.notification";
 	private static final String PATH = "/sys/class/leds/wled:backlight/brightness";
+	private static final String ZERO_BRIGHTNESS = "0";
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-registerReceiver(new BroadcastReceiver() {
+		registerReceiver(new BroadcastReceiver() {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		Toast.makeText(getApplicationContext(), "Я проснулся", Toast.LENGTH_SHORT).show(); 
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				Toast.makeText(getApplicationContext(), "Я проснулся", Toast.LENGTH_SHORT).show();
+			}
+		}, new IntentFilter(Intent.ACTION_SCREEN_ON));
 	}
-},
-new IntentFilter(Intent.ACTION_SCREEN_ON));
-}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		createNotification();
-		String intentAction = intent.getAction(); 
+		String brightnessLevel = getCurrentBrightnessLevel();
+		createNotification(brightnessLevel);
+		String intentAction = intent.getAction();
 		if (NOTIFICATION_ACTION.equals(intentAction)) {
-handleNotificationClick();
+			handleNotificationClick();
 		}
 		return super.onStartCommand(intent, flags, startId);
 	}
 
-	private void createNotification() {
-		CharSequence t = getResources().getString(R.string.switch_off);
+	private void createNotification(String brightnessLevel) {
+		CharSequence t = "";
+		if (brightnessLevel.equals(ZERO_BRIGHTNESS)) {
+			t = getResources().getString(R.string.switch_on);
+		} else {
+			t = getResources().getString(R.string.switch_off);
+		}
 		Context c = getApplicationContext();
 		Intent i = new Intent(NOTIFICATION_ACTION, null, c, BacklightSwitchService.class);
 		PendingIntent pi = PendingIntent.getService(c, 0, i, 0);
@@ -57,20 +63,23 @@ handleNotificationClick();
 	}
 
 	private void handleNotificationClick() {
-			Toast.makeText(getApplicationContext(), "Получаю интент", Toast.LENGTH_SHORT).show();
-			defineSwitchOperation();
-			// switchBacklight();
-		}
-
-	private void defineSwitchOperation() {
-		String brightnessLevel = getCurrentBrightnessLevel();
-		boolean backlightIsCurrentlyOff = isBacklightCurrentlyOff(brightnessLevel);
-		String brightnessLevelToBeRestored = getBrightnessLevelToBeRestored(backlightIsCurrentlyOff, brightnessLevel);
-//DimmerView dimmer_view = getDimmerView();
-//LayoutParams dimmer_params = defineDimmerParams();
-		String notificationText = getNotificationText(backlightIsCurrentlyOff);
-//switchBacklight(backlightIsCurrentlyOff, brightnessLevelToBeRestored, dimmer_view, dimmer_params, notificationText);
+		Toast.makeText(getApplicationContext(), "Получаю интент", Toast.LENGTH_SHORT).show();
+		// defineSwitchOperation();
+		// switchBacklight();
 	}
+
+	// private void defineSwitchOperation() {
+	// String brightnessLevel = getCurrentBrightnessLevel();
+	// boolean backlightIsCurrentlyOff =
+	// isBacklightCurrentlyOff(brightnessLevel);
+	// String brightnessLevelToBeRestored =
+	// getBrightnessLevelToBeRestored(backlightIsCurrentlyOff, brightnessLevel);
+	// DimmerView dimmer_view = getDimmerView();
+	// LayoutParams dimmer_params = defineDimmerParams();
+	// String notificationText = getNotificationText(backlightIsCurrentlyOff);
+	// switchBacklight(backlightIsCurrentlyOff, brightnessLevelToBeRestored,
+	// dimmer_view, dimmer_params, notificationText);
+	// }
 
 	private String getCurrentBrightnessLevel() {
 		File f = new File(PATH);
@@ -89,8 +98,7 @@ handleNotificationClick();
 	}
 
 	private boolean isBacklightCurrentlyOff(String brightnessLevel) {
-		String zeroBrightness = "0";
-		boolean backlightIsCurrentlyOff = brightnessLevel.equals(zeroBrightness);
+		boolean backlightIsCurrentlyOff = brightnessLevel.equals(ZERO_BRIGHTNESS);
 		return backlightIsCurrentlyOff;
 	}
 
@@ -102,46 +110,47 @@ handleNotificationClick();
 		return brightnessLevelToBeRestored;
 	}
 
-private DimmerView getDimmerView() {
-	DimmerView dimmer_view = new DimmerView(this);
-	return dimmer_view;
-}
+	private DimmerView getDimmerView() {
+		DimmerView dimmer_view = new DimmerView(this);
+		return dimmer_view;
+	}
 
-private LayoutParams defineDimmerParams() {
-	WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
-	LayoutParams dimmer_params = new LayoutParams();
-	dimmer_params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
-	dimmer_params.flags |= LayoutParams.FLAG_DIM_BEHIND; 
-	dimmer_params.dimAmount = 1.0f;
-	dimmer_params.flags |= LayoutParams.FLAG_NOT_FOCUSABLE;
-	dimmer_params.flags |= LayoutParams.FLAG_NOT_TOUCHABLE;
-dimmer_params.flags |= LayoutParams.FLAG_FULLSCREEN;
-	dimmer_params.flags &= ~LayoutParams.FLAG_KEEP_SCREEN_ON;
+	private LayoutParams defineDimmerParams() {
+		WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+		LayoutParams dimmer_params = new LayoutParams();
+		dimmer_params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+		dimmer_params.flags |= LayoutParams.FLAG_DIM_BEHIND;
+		dimmer_params.dimAmount = 1.0f;
+		dimmer_params.flags |= LayoutParams.FLAG_NOT_FOCUSABLE;
+		dimmer_params.flags |= LayoutParams.FLAG_NOT_TOUCHABLE;
+		dimmer_params.flags |= LayoutParams.FLAG_FULLSCREEN;
+		dimmer_params.flags &= ~LayoutParams.FLAG_KEEP_SCREEN_ON;
 		dimmer_params.flags &= ~LayoutParams.FLAG_TURN_SCREEN_ON;
-	dimmer_params.format = PixelFormat.OPAQUE;
+		dimmer_params.format = PixelFormat.OPAQUE;
 
-	Point p = new Point();
-	wm.getDefaultDisplay().getRealSize(p);
-	dimmer_params.width = p.x;
-	dimmer_params.height = p.y;
+		Point p = new Point();
+		wm.getDefaultDisplay().getRealSize(p);
+		dimmer_params.width = p.x;
+		dimmer_params.height = p.y;
 
-return dimmer_params;
-}
+		return dimmer_params;
+	}
 
 	private String getNotificationText(boolean backlightIsCurrentlyOff) {
 		String notificationText = "";
 		if (backlightIsCurrentlyOff) {
-			notificationText = getResources().getString(R.string.switch_off);
-		} else {
 			notificationText = getResources().getString(R.string.switch_on);
+		} else {
+			notificationText = getResources().getString(R.string.switch_off);
 		}
 		return notificationText;
 	}
 
-	private void switchBacklight(boolean backlightIsCurrentlyOff, String brightnessLevelToBeRestored, DimmerView dimmer_view, LayoutParams dimmer_params, String notificationText) {
+	private void switchBacklight(boolean backlightIsCurrentlyOff, String brightnessLevelToBeRestored,
+			DimmerView dimmer_view, LayoutParams dimmer_params, String notificationText) {
 		if (backlightIsCurrentlyOff) {
 			switchBacklightOn(brightnessLevelToBeRestored);
-//removeDimmerOutOfTheScreen(dimmer_view);
+			// removeDimmerOutOfTheScreen(dimmer_view);
 			updateNotification(notificationText);
 		} else {
 			switchBacklightOff();
@@ -159,7 +168,7 @@ return dimmer_params;
 	private void putDimmerOverTheScreen(DimmerView dimmer_view, LayoutParams dimmer_params) {
 		WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
 		wm.addView(dimmer_view, dimmer_params);
-}
+	}
 
 	private void removeDimmerOutOfTheScreen(DimmerView dimmer_view) {
 		WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
@@ -178,7 +187,7 @@ return dimmer_params;
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO: Return the communication channel to the service.
-		//throw new UnsupportedOperationException("Not yet implemented");
-return null;
+		// throw new UnsupportedOperationException("Not yet implemented");
+		return null;
 	}
 }
